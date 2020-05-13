53693f84728d553ce4eb532dacc90d20f96c2a3e
Ivan Pastukhov <spirithntr@gmail.com>
2020-05-13 10:26:21 +0400

    fix: remove multiple roots
    move components into App

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -4,6 +4,6 @@ import { shallow } from 'enzyme';
 import { App } from '../App';
 
 it('renders the heading', () => {
-  const result = shallow(<App />).containsMatchingElement(<h1>Hello World!</h1>);
+  const result = shallow(<App />).containsMatchingElement(<h1 className="container">Hello world!</h1>);
   expect(result).toBeTruthy();
 });
