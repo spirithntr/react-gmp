422db515358fc4f990a8f10bc6fa5e3d29d1bc0e
Ivan Pastukhov <spirithntr@gmail.com>
2020-05-22 02:05:32 +0400

    feat: add app state

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,38 @@ import { InfoSplit } from './components/InfoSplit/InfoSplit';
 
 import { data as moviesMock } from '../__mocks__/movies.json';
 import { SearchSplit } from './components/SearchSplit/SearchSplit';
+import { Movie, SearchTabs, SortTabs } from './models/movies';
 
 const movie = moviesMock[7];
 
-export const App = () => (
-  <ErrorBoundary>
-    <SearchPanel></SearchPanel>
-    <SearchSplit />
-    <InfoPanel movie={movie} />
-    <InfoSplit genre={movie.genres[0]} />
-    <MovieList></MovieList>
-    <Footer />
-  </ErrorBoundary>
-);
+type State = {
+  selectedMovie: Movie | null;
+  movies: Movie[];
+  searchTab: SearchTabs;
+  sortTab: SortTabs;
+}
+
+export class App extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      sortTab: SortTabs.rating,
+      searchTab: SearchTabs.genre,
+      movies: moviesMock,
+      selectedMovie: null
+    }
+  }
+  render() {
+    return (
+      <ErrorBoundary>
+        <SearchPanel></SearchPanel>
+        <SearchSplit />
+        <InfoPanel movie={movie} />
+        <InfoSplit genre={movie.genres[0]} />
+        <MovieList />
+        <Footer />
+      </ErrorBoundary>
+    )
+  }
+}
