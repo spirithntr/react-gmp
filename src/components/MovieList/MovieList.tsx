8f0aac148796ecabb77ec830681ab60e64aacca0
Ivan Pastukhov <spirithntr@gmail.com>
2020-05-21 13:50:34 +0400

    feat: add movielist

---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MovieList.scss';
+import { Movie } from '../Movie/Movie';
+
+import { data as moviesMock } from '../../../__mocks__/movies.json';
+
+const movies = moviesMock.map((movie) => (
+  <Movie
+    key={movie.id}
+    poster={movie.poster_path}
+    title={movie.title}
+    year={movie.release_date}
+    genres={movie.genres}
+  />
+));
+
+export const MovieList: React.FunctionComponent<any> = (props) => <div className="movielist">{movies}</div>;
