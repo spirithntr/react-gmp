0ef0fc9e3b23e4c7844e3413ae177e44b34aad0e
Ivan Pastukhov <spirithntr@gmail.com>
2020-05-21 22:39:24 +0400

    feat: add searchbutton

---
+++ src/components/SearchButton/SearchButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchButton.scss';
+
+export const SearchButton = (props: any) => {
+  return (
+    <div className="icon-container">
+      <div className="search-icon"></div>
+    </div>
+  );
+};
