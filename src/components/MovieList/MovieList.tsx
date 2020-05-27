a39a2bcae468fa467b7fa2c3cbe806d14090da7d
Ivan Pastukhov <spirithntr@gmail.com>
2020-05-28 00:44:28 +0400

    test: add more tests

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -8,23 +8,20 @@ import { data as moviesMock } from '../../../__mocks__/movies.json';
 type Props = {
   onSelect: any;
   movies: IMovie[];
-}
-
-
+};
 
 export const MovieList: React.FunctionComponent<Props> = (props) => {
-  const makeList = (movies: IMovie[]) => props.movies.map((movie) => (
-    <Movie
-      id={movie.id}
-      genres={movie.genres}
-      key={movie.id}
-      poster={movie.poster_path}
-      title={movie.title}
-      year={movie.release_date.split('-').shift()}
-      onSelect={props.onSelect}
-    />
-  ));
-  return (<div className="movielist">
-    {makeList(props.movies)}
-  </div>)
+  const makeList = (movies: IMovie[]) =>
+    props.movies.map((movie) => (
+      <Movie
+        id={movie.id}
+        genres={movie.genres}
+        key={movie.id}
+        poster={movie.poster_path}
+        title={movie.title}
+        year={movie.release_date.split('-').shift()}
+        onSelect={props.onSelect}
+      />
+    ));
+  return <div className="movielist">{makeList(props.movies)}</div>;
 };
