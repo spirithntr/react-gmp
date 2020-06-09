ab1da9246daa39141b4839348fa9a4afc6ccab16
Ivan Pastukhov <ivan_pastukhov@epam.com>
2020-06-09 20:33:36 +0400

    fix: test

--- src/components/SearchSplit/SearchSplit.spec.tsx
+++ src/components/SearchSplit/SearchSplit.spec.tsx
@@ -8,11 +8,8 @@ import { SortTabs } from '../../models/movies';
 const mockSelectHandler = jest.fn((x) => void 0) as any;
 
 describe('SearchSplit', () => {
-    it('should call handler on click', () => {
-        const component = render(
-            <SearchSplit onToggle={mockSelectHandler} sortTab={SortTabs.rating} />
-        );
-        expect(component.find('.split-line__found').text()).toEqual('7 movies found');
-
-    });
+  it('should call handler on click', () => {
+    const component = render(<SearchSplit moviesCount={7} onToggle={mockSelectHandler} sortTab={SortTabs.rating} />);
+    expect(component.find('.split-line__found').text()).toEqual('7 movies found');
+  });
 });
