964a0d613186c481abb5d5d92b9147b954dbbbcf
Ivan Pastukhov <spirithntr@gmail.com>
2020-06-26 21:10:27 +0400

    fix: movies counter

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -5,6 +5,8 @@ import { SearchSplit } from '../SearchSplit/SearchSplit';
 import { Movie, SortTabs, SearchTabs, State } from '../../models/movies';
 import { connect } from 'react-redux';
 import * as Actions from '../../store/actions/actions';
+import { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
 
 type StateProps = {
   movies: Movie[];
@@ -60,7 +62,7 @@ class BasicSearchPage extends React.Component<Props> {
           onClick={this.searchMovies}
         ></SearchPanel>
         <SearchSplit
-          moviesCount={this.props.movies.length}
+          moviesCount={this.props?.movies?.length || 0}
           sortTab={this.props.sortTab}
           onToggle={this.props.switchSortTab}
         ></SearchSplit>
@@ -76,7 +78,7 @@ const mapStateToProps = (state: State): StateProps => ({
   search: state.search,
 });
 
-const mapDispatchToProps = (dispatch: any): DispatchProps => {
+const mapDispatchToProps = (dispatch: ThunkDispatch<State, any, Action>): DispatchProps => {
   return {
     getMovies: () => dispatch(Actions.getMoviesAction()),
     switchSearchTab: (searchTab: SearchTabs) => dispatch(Actions.switchSearchTabAction(searchTab)),
